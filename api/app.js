e55ae68c1be01d51382394231b0a85087ebd9e22
FreemindNation <freemindnation@outlook.com>
2024-05-28 16:58:45 +0100

    Remove unused middlewares

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { getTopics } = require('../api/controllers/topics-controllers');
-const { handleCustomErrors, handleGeneric404Errors } = require('./error-handling/handle-errors');
+const { handleCustomErrors, handleGeneric404Errors, handleServerErrors } = require('./error-handling/handle-errors');
 
 const app = express();
 
 
-app.use(express.json());
+
 
 app.get('/api/topics', getTopics);
 
@@ -15,9 +15,11 @@ app.get('/api/topics', getTopics);
 
 
 
-app.use(handleCustomErrors);
+
 
 app.all('*', handleGeneric404Errors);
+
+app.use(handleServerErrors);
 
 
 
