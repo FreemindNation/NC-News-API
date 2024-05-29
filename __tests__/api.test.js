83306a05044b20feac21287f4b441749fb9beed6
FreemindNation <freemindnation@outlook.com>
2024-05-29 09:43:23 +0100

    Add tests for /api endpoint

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -3,6 +3,8 @@ const testData = require('../db/data/test-data/index');
 const db = require('../db/connection');
 const request = require('supertest');
 const app = require('../api/app');
+const apiEndpoints = require('../endpoints.json');
+
 
 
 beforeEach(() => seed(testData));
@@ -15,7 +17,8 @@ describe('/api/topics', () => {
         .get('/api/topics')
         .expect(200)
         .then(({ body })=> {
-            const topics = body.rows;
+            const  { topics }  = body;
+            
             expect(topics).toHaveLength(3)
             topics.forEach((topic)=> {
                 expect(topic).toMatchObject({
@@ -36,3 +39,28 @@ describe('/api/topics', () => {
         })
     });
 });
+
+describe('/api', () => {
+    test('200: responds with an object of all available endpoints', () => {
+      return request(app)
+      .get('/api')
+      .expect(200)
+      .then(({ body })=> {
+        const { endpoints } = body;
+        expect(endpoints).toEqual(apiEndpoints);
+      })
+    });
+
+    test('404: responds with "Route not found" if passed an invalid route', () => {
+        return request(app)
+        .get('/not-a-valid-route')
+        .expect(404)
+        .then(({ body })=> {
+            expect(body.msg).toBe('Route not found');
+        })
+    });
+});
+
+
+
+
