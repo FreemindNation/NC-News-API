ece47abe8f5239c5958a5c53cec341b0accfe77c
FreemindNation <freemindnation@outlook.com>
2024-09-21 16:01:48 +0100

    Edit api.test.js to add tests for /api/users/:username endpoint. Edit users models, controllers and router to add functionality for /api/users/:username endpoint

--- api/controllers/users-controllers.js
+++ api/controllers/users-controllers.js
@@ -1,4 +1,4 @@
-const { selectUsers } = require("../models/users-models")
+const { selectUsers, selectUserByUsername } = require("../models/users-models")
 
 
 
@@ -7,4 +7,13 @@ exports.getUsers = (req, res, next)=> {
     .then((users)=> {
         res.status(200).send({ users });
     })
+}
+
+exports.getUserByUsername = (req, res, next)=> {
+    const { username } = req.params;
+    selectUserByUsername(username)
+    .then((user)=> {
+        res.status(200).send({ user })
+    })
+    .catch(next)
 }
